fa17b67da35f8bbfab17703efd8e85c96c3bace9
y1guo <guoyi0328@gmail.com>
2024-03-25 04:19:06 -0700

    feat: add remux

--- auto_transcode/modules/remux.py
+++ auto_transcode/modules/remux.py
@@ -1,6 +1,11 @@
+import os
+import shutil
+
+import ffmpeg
+
 from auto_transcode.modules.base import WatcherProcess
-from auto_transcode.utils.logger import get_logger
 from auto_transcode.settings import Settings
+from auto_transcode.utils.logger import get_logger
 
 
 logger = get_logger(__name__)
@@ -9,17 +14,84 @@ logger = get_logger(__name__)
 class RemuxProcess(WatcherProcess):
     def __init__(self):
         super().__init__(process_name="Remux")
-        self.flv_dirs = Settings.FLV_DIRS
-        self.remux_dir = Settings.REMUX_DIR
-        self.days_before_remux = Settings.DAYS_BEFORE_REMUX
 
     def main(self):
-        for flv_dir in self.flv_dirs:
+        for flv_dir in Settings.FLV_DIRS:
             self.watch(
                 dir=flv_dir,
-                delay=self.days_before_remux * 86400,
-                callback=self.remux,
+                delay=Settings.DAYS_BEFORE_REMUX * 86400,
+                callback=self.callback,
             )
 
-    def remux(self, file_path: str):
-        logger.info(f"Remuxing: {file_path}")
+    def callback(self, flv_path: str):
+        dir, filename = os.path.split(flv_path)
+        basename, ext = os.path.splitext(filename)
+
+        # check that file is FLV
+        if ext != ".flv":
+            logger.error(f"File {flv_path} is not an FLV file")
+            return
+
+        mp4_path = os.path.join(Settings.CACHE_DIR, f"{basename}.mp4")
+        xml_from = os.path.join(dir, f"{basename}.xml")
+        xml_to = os.path.join(Settings.CACHE_DIR, f"{basename}.xml")
+
+        logger.info(f"Remuxing {repr(flv_path)} to {repr(mp4_path)}")
+        self.remux(flv_path, mp4_path)
+        self.copy_xml(xml_from, xml_to)
+        # TODO: validate mp4 and xml
+        # TODO: remove flv and original xml
+
+    def remux(self, flv_path: str, mp4_path: str) -> None:
+        # check if already remuxed
+        try:
+            flv_duration = float(ffmpeg.probe(flv_path)["format"]["duration"])
+            mp4_duration = float(ffmpeg.probe(mp4_path)["format"]["duration"])
+        except Exception:
+            pass
+        else:
+            if abs(flv_duration - mp4_duration) < 1:
+                logger.info(f"File {repr(flv_path)} is already remuxed")
+                return
+        # remux
+        try:
+            # ffmpeg.input(flv_path).output(mp4_path, c="copy").run(overwrite_output=True, quiet=True)
+            ffmpeg.input(flv_path).output(mp4_path, c="copy").run(
+                overwrite_output=True, capture_stdout=True, capture_stderr=True
+            )
+        except ffmpeg.Error as e:
+            logger.error(f"Failed to remux {repr(flv_path)}")
+            print("stdout:", e.stdout.decode("utf8"))
+            print("stderr:", e.stderr.decode("utf8"))
+        else:
+            logger.info(f"Remuxed {repr(flv_path)} to {repr(mp4_path)}")
+
+    def copy_xml(self, xml_from: str, xml_to: str) -> None:
+        try:
+            shutil.copyfile(xml_from, xml_to)
+        except FileNotFoundError:
+            logger.error(f"File {repr(xml_from)} not found")
+        except FileExistsError:
+            logger.error(f"File {repr(xml_to)} already exists")
+        except IsADirectoryError:
+            logger.error(f"File {repr(xml_from)} is a directory")
+        except PermissionError:
+            logger.error(f"Permission denied to copy {repr(xml_from)}")
+        except Exception as e:
+            logger.error(f"Failed to copy {repr(xml_from)}: {e}")
+        else:
+            logger.info(f"Copied {repr(xml_from)} to {repr(xml_to)}")
+
+    def remove(self, file_path: str):
+        try:
+            os.remove(file_path)
+        except FileNotFoundError:
+            logger.error(f"File {repr(file_path)} not found")
+        except IsADirectoryError:
+            logger.error(f"File {repr(file_path)} is a directory")
+        except PermissionError:
+            logger.error(f"Permission denied to remove {repr(file_path)}")
+        except Exception as e:
+            logger.error(f"Failed to remove {repr(file_path)}: {e}")
+        else:
+            logger.info(f"Removed {repr(file_path)}")
