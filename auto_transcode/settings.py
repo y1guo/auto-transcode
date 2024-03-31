e3baaa64c433ce65bb76c735b90defcea3bea53f
y1guo <guoyi0328@gmail.com>
2024-03-30 18:55:46 -0700

    feat: add env MINIMUM_FILE_SIZE and MINIMUM_VIDEO_DURATION

--- auto_transcode/settings.py
+++ auto_transcode/settings.py
@@ -16,6 +16,8 @@ class Settings:
     REMUX_DIR: str
     SAVE_DIR: str
     CACHE_DIR: str
+    MINIMUM_FILE_SIZE: int
+    MINIMUM_VIDEO_DURATION: float
     DAYS_BEFORE_REMUX: float
     DAYS_BEFORE_TRANSCODE: float
     WAKEUP_TIME: float
@@ -26,6 +28,8 @@ class Settings:
         REMUX_DIR = cls.load_dir("REMUX_DIR")
         SAVE_DIR = cls.load_dir("SAVE_DIR")
         CACHE_DIR = cls.load_dir("CACHE_DIR")
+        MINIMUM_FILE_SIZE = cls.load_non_negative_int("MINIMUM_FILE_SIZE")
+        MINIMUM_VIDEO_DURATION = cls.load_non_negative_float("MINIMUM_VIDEO_DURATION")
         DAYS_BEFORE_REMUX = cls.load_non_negative_float("DAYS_BEFORE_REMUX")
         DAYS_BEFORE_TRANSCODE = cls.load_non_negative_float("DAYS_BEFORE_TRANSCODE")
         WAKEUP_TIME = cls.load_non_negative_float("WAKEUP_TIME", "60")
@@ -35,6 +39,8 @@ class Settings:
             or REMUX_DIR is None
             or SAVE_DIR is None
             or CACHE_DIR is None
+            or MINIMUM_FILE_SIZE is None
+            or MINIMUM_VIDEO_DURATION is None
             or DAYS_BEFORE_REMUX is None
             or DAYS_BEFORE_TRANSCODE is None
             or WAKEUP_TIME is None
@@ -46,6 +52,8 @@ class Settings:
         cls.REMUX_DIR = REMUX_DIR
         cls.SAVE_DIR = SAVE_DIR
         cls.CACHE_DIR = CACHE_DIR
+        cls.MINIMUM_FILE_SIZE = MINIMUM_FILE_SIZE
+        cls.MINIMUM_VIDEO_DURATION = MINIMUM_VIDEO_DURATION
         cls.DAYS_BEFORE_REMUX = DAYS_BEFORE_REMUX
         cls.DAYS_BEFORE_TRANSCODE = DAYS_BEFORE_TRANSCODE
         cls.WAKEUP_TIME = WAKEUP_TIME
@@ -91,7 +99,20 @@ class Settings:
         try:
             res = float(value)
         except ValueError:
-            logger.critical(f"{var_name}={value} is not a number")
+            logger.critical(f"{var_name}={value} is not a float number")
+            return
+        else:
+            return res
+
+    @classmethod
+    def load_int(cls, var_name: str, default: str | None = None):
+        value = cls.load_str(var_name, default)
+        if value is None:
+            return
+        try:
+            res = int(value)
+        except ValueError:
+            logger.critical(f"{var_name}={value} is not an integer number")
             return
         else:
             return res
@@ -106,6 +127,16 @@ class Settings:
             return
         return value
 
+    @classmethod
+    def load_non_negative_int(cls, var_name: str, default: str | None = None):
+        value = cls.load_int(var_name, default)
+        if value is None:
+            return
+        if value < 0:
+            logger.critical(f"{var_name}={value} must be greater or equal to 0")
+            return
+        return value
+
 
 if __name__ == "__main__":
     Settings.init()
@@ -113,6 +144,8 @@ if __name__ == "__main__":
     logger.info(f"REMUX_DIR: {Settings.REMUX_DIR}")
     logger.info(f"SAVE_DIR: {Settings.SAVE_DIR}")
     logger.info(f"CACHE_DIR: {Settings.CACHE_DIR}")
+    logger.info(f"MINIMUM_FILE_SIZE: {Settings.MINIMUM_FILE_SIZE}")
+    logger.info(f"MINIMUM_VIDEO_DURATION: {Settings.MINIMUM_VIDEO_DURATION}")
     logger.info(f"DAYS_BEFORE_REMUX: {Settings.DAYS_BEFORE_REMUX}")
     logger.info(f"DAYS_BEFORE_TRANSCODE: {Settings.DAYS_BEFORE_TRANSCODE}")
     logger.info(f"WAKEUP_TIME: {Settings.WAKEUP_TIME}")
